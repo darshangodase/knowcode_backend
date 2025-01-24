ffea076868e85a396e209998b276c08e86dbd890
Darshan Godase <darshangodase10@example.com>
2025-01-24 22:28:54 +0530

    added bid system and ewaste system

--- routes/rewardRoutes.js
+++ routes/rewardRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+// const authMiddleware = require('../middleware/authMiddleware');
 const { getRewards, claimReward } = require('../controllers/rewardController');
 
-router.get('/', authMiddleware, getRewards);
-router.post('/claim', authMiddleware, claimReward);
+router.get('/', getRewards);
+router.post('/claim', claimReward);
 
 module.exports = router;
